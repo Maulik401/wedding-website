5e609105c11b7fdf8284beedd59a73948a5202fd
Maulik401 <45719557+Maulik401@users.noreply.github.com>
2025-05-04 15:47:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ st.write("""
 st.header("Venue Location")
 st.components.v1.html("""
 <iframe
-  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3672.538664777448!2d72.571362!3d23.030357!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e84f2aafeabe1%3A0x9e4e4099b1a3e8f7!2sAhmedabad%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1713623200000!5m2!1sen!2sin"
+  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3722.5794784045016!2d70.36644287504104!3d20.90878848076268!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be2a46bdbb3a9df%3A0x1e3d9b3486375901!2sVisawadia%20Vadi%2C%20Veraval%2C%20Gujarat!5e0!3m2!1sen!2sin!4v1714826138056!5m2!1sen!2sin"
   width="100%" height="400" style="border:0;" allowfullscreen="" loading="lazy">
 </iframe>
 """, height=400)
